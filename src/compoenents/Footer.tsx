8243fd4cc73b7070ef85570016a0248885aa373c
Shubham Jayswal <shubhammasai03@gmail.com>
2024-04-09 15:21:23 +0530

    Added responsive Navbar and Footer components

--- src/compoenents/Footer.tsx
+++ src/compoenents/Footer.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
-
-const Footer:React.FC= () => {
+import Facebook from "../assets/facebook.svg"
+import Instagram from "../assets/instagram.svg"
+import twitter from "../assets/twitter.svg"
+const Footer: React.FC = () => {
   return (
-    <div>Footer</div>
+    <div className='w-[100%] bg-black text-white flex flex-col items-center gap-[10px] p-[1.2rem] md:flex-row md:justify-between md:px-[3.5rem]' >
+      <p>
+        Copyright Ⓒ 2024 hypergro. All Rights Reserved.
+      </p>
+      <div className='flex gap-[1rem]'>
+        <img src={Facebook} alt="Facebook-logo" />
+        <img src={Instagram} alt="Instagram-logo" />
+        <img src={twitter} alt="twitter-logo" />
+      </div>
+    </div>
   )
 }
 
--- src/compoenents/Navbar.tsx
+++ src/compoenents/Navbar.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import LogoSVG from "../assets/app_logo.svg"
 const Navbar: React.FC = () => {
     return (
-        <div className='w-[100%] bg-black text-white p-[1.2rem] px-[1.8rem] md:px-[3.5rem]'>
-            <img src={LogoSVG} alt="Hypergro-logo" />
+        <div className='w-[100%] bg-black p-[1.2rem] px-[1.8rem] md:px-[3.5rem]'>
+            <img src={LogoSVG} alt="Hypergro-logo" className='w-[6rem] md:w-[10rem]' />
         </div>
     )
 }
